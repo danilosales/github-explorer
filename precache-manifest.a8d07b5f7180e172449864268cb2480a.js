9e516150338b7e8a1af3b65bf4b99ab5dd64b736
danilosales <danilosales.info@gmail.com>
2020-09-13 17:21:36 +0000

    Deploying to gh-pages from  @ e8ec79d1a950d9802ebee361d2fdf5d963a39917 🚀

---
+++ precache-manifest.a8d07b5f7180e172449864268cb2480a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e337a2e64159ca92bc7f5a7e71dd38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48604f2beed2c3c5e533",
+    "url": "/static/js/2.d4dd438b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4dd438b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68daf8ef5310812ad121",
+    "url": "/static/js/main.be58b138.chunk.js"
+  },
+  {
+    "revision": "23a8542c495de0a29ff2",
+    "url": "/static/js/runtime-main.f51e40a7.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/githubBackground.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
